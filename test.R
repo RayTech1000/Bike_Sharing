1f28539a652e6a3069b825ade95a2c6a597d8126
Ray Chun <75832329+RayTech1000@users.noreply.github.com>
2023-03-13 10:48:36 -0500

    Add files via upload

---
+++ test.R
@@ -0,0 +1,8 @@
+bike_sharing
+A visual analysis of a public bike sharing system in R.
+
+This analysis is done on a bike sharing and rental system between years 2011 and 2012.
+
+Data includes details on season, weather, windspeed, user counts, and more.
+
+Data Science features include data exploration, visuals, etc.
